4c42bcbe1608d8e8c295d43e810e27bd400b6eb8
Siracenco Serghei <siracencoserghei@gmail.com>
2025-12-14 22:39:52 +0100

    Restricting the Root URL Pattern

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 "net/http"
 )
 func home(w http.ResponseWriter, r *http.Request) {
+// Check if the current request URL path exactly matches "/". If it doesn't, use
+// the http.NotFound() function to send a 404 response to the client.
+// Importantly, we then return from the handler. If we don't return the handler
+// would keep executing and also write the "Hello from SnippetBox" message.
+if r.URL.Path != "/" {
+http.NotFound(w, r)
+return
+}
 w.Write([]byte("Hello from Snippetbox"))
 }
 // Add a showSnippet handler function.
