ddec2b44519fc9fcabf463d981c7cc54ed666b12
Siracenco Serghei <siracencoserghei@gmail.com>
2026-02-01 21:09:07 +0100

    explicitly return

--- examples/named_return_values.go
+++ examples/named_return_values.go
@@ -16,3 +16,42 @@ func yearsUntilEvents(age int) (yearsUntilAdult, yearsUntilDrinking, yearsUntilC
 	return
 }
 
+// ======================
+//  Explicitly Return
+// ======================
+
+// func yearsUntilEvents(age int) (int, int, int) {
+// 	yearsUntilAdult := 18 - age
+// 	if yearsUntilAdult < 0 {
+// 		yearsUntilAdult = 0
+// 	}
+// 	yearsUntilDrinking := 21 - age
+// 	if yearsUntilDrinking < 0 {
+// 		yearsUntilDrinking = 0
+// 	}
+// 	yearsUntilCarRental := 25 - age
+// 	if yearsUntilCarRental < 0 {
+// 		yearsUntilCarRental = 0
+// 	}
+// 	return yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental
+// }
+
+
+//  or 
+
+// func yearsUntilEvents(age int) (yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental int) {
+// 	yearsUntilAdult = 18 - age
+// 	if yearsUntilAdult < 0 {
+// 		yearsUntilAdult = 0
+// 	}
+// 	yearsUntilDrinking = 21 - age
+// 	if yearsUntilDrinking < 0 {
+// 		yearsUntilDrinking = 0
+// 	}
+// 	yearsUntilCarRental = 25 - age
+// 	if yearsUntilCarRental < 0 {
+// 		yearsUntilCarRental = 0
+// 	}
+// 	return yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental
+// }
+
